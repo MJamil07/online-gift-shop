620ec31796bff7e31e2f505197704522d90a49dd
MJamil07 <kingivan223@gmail.com>
2024-02-12 12:46:53 +0530

    UI ( Changed ) for login and signup

--- frondend/gift-shop/src/online-shop/auth/Login.tsx
+++ frondend/gift-shop/src/online-shop/auth/Login.tsx
@@ -1,65 +1,78 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import {
+       MDBCol,
        MDBContainer,
        MDBInput,
-       
+       MDBRow,
+
 } from 'mdb-react-ui-kit';
 import { Link } from 'react-router-dom';
-import axios, { AxiosError } from 'axios'
+import axios from 'axios'
 import URL from '../utils/url';
 import { notification } from 'antd'
+import Gift from '../../images/categorie/gift.png'
 
 
-export default function Login() : JSX.Element {
+export default function Login(): JSX.Element {
 
-       const [email , setEmail] = useState<string>('')
-       const [password , setPassword] = useState<string>('')
-       const [api , contextHolder] = notification.useNotification()
+       const [email, setEmail] = useState<string>('')
+       const [password, setPassword] = useState<string>('')
+       const [api, contextHolder] = notification.useNotification()
 
        const handleLogin = async () => {
               try {
                      const response = await axios.post(`${URL}/user/login`, { email, password });
                      if (response.status === 200) {
                             api.success({ message: 'Login Successful' });
-                            localStorage.setItem('userId' , response.data?.user?._id)
+                            localStorage.setItem('userId', response.data?.user?._id)
                             window.location.assign('/')
                      }
               } catch (error) {
-                     api.error({ message: 'Login Failed'});
+                     api.error({ message: 'Login Failed' });
                      console.log(error);
               }
-              
+
        }
 
        return (
-              <MDBContainer className="p-3 my-5 d-flex flex-column w-25 ">
+              <MDBContainer style={{backgroundColor : 'white' , padding : '50px' , marginTop : '150px' }} >
                      {contextHolder}
-                     <h2 className="mb-4">Login</h2>
-                     
-                     <MDBInput 
-                            wrapperClass='mb-4' 
-                            label='Email address' 
-                            id='form1' 
-                            type='email'
-                            value={email}
-                            required = { true }
-                            onChange={(e) => setEmail(e.target.value)}
-                     />
-                     <MDBInput 
-                            wrapperClass='mb-4' 
-                            label='Password' 
-                            id='form2' 
-                            type='password'
-                            required = { true }
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                     />
-                     <button onClick={handleLogin} className="mb-4 btn btn-primary">Sign in</button>
+                     <MDBRow className="justify-content-center">
+                            <MDBCol md="8" lg="6" className="text-center">
+                                   <img width="70" height="70" src= {Gift} alt="Logo" className="mb-4" />
+                                   <h2 className="mb-4">Gift.io</h2>
+                            </MDBCol>
+                     </MDBRow>
+                     <MDBRow  className="justify-content-center">
+                            <MDBCol md="8" lg="6">
+                                   <h3 className="mb-4">Login</h3>
+                                   <MDBInput
+                                          wrapperClass='mb-4'
+                                          label='Email address'
+                                          id='form1'
+                                          type='email'
+                                          value={email}
+                                          required={true}
+                                          onChange={(e) => setEmail(e.target.value)}
+                                   />
+                                   <MDBInput
+                                          wrapperClass='mb-4'
+                                          label='Password'
+                                          id='form2'
+                                          type='password'
+                                          required={true}
+                                          value={password}
+                                          onChange={(e) => setPassword(e.target.value)}
+                                   />
+                                   <button onClick={handleLogin} className="mb-4 btn btn-primary">Login in with Gift.io</button>
 
-                     <div className="text-center">
-                            <p>Not a member? <Link to="/signup">SignUp</Link></p>
-                     </div>
-
+                                   <div className="text-center">
+                                          <p>Not a member? <Link to="/signup">Sign Up</Link></p>
+                                   </div>
+                            </MDBCol>
+                     </MDBRow>
               </MDBContainer>
+
+
        )
 }
--- frondend/gift-shop/src/online-shop/auth/SignUp.tsx
+++ frondend/gift-shop/src/online-shop/auth/SignUp.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import { MDBContainer, MDBInput } from 'mdb-react-ui-kit';
+import { MDBCol, MDBContainer, MDBInput, MDBRow } from 'mdb-react-ui-kit';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import URL from '../utils/url';
 import { notification } from 'antd';
-
+import Gift from '../../images/categorie/gift.png'
 export type UserType = {
        username: string;
        email: string;
@@ -20,15 +20,15 @@ export default function SignUp(): JSX.Element {
               password: '',
        });
        const [api, contextHolder] = notification.useNotification();
-       
+
        const handleLogin = async () => {
               console.log(user);
-              
+
               try {
                      const response = await axios.post(`${URL}/user/register`, user);
                      if (response.status === 201) {
                             api.success({ message: 'Signup Successful' });
-                            localStorage.setItem('userId' , response.data?.user?._id)
+                            localStorage.setItem('userId', response.data?.user?._id)
                             window.location.assign('/')
                      }
               } catch (error) {
@@ -46,62 +46,68 @@ export default function SignUp(): JSX.Element {
        };
 
        return (
-              <MDBContainer className="p-3 my-5 d-flex flex-column w-25 ">
+              <MDBContainer style={{ backgroundColor: 'white', padding: '50px', marginTop: '50px' }}>
                      {contextHolder}
-                     <h2 className="mb-4">Sign Up</h2>
-
-                     <MDBInput
-                            wrapperClass="mb-4"
-                            label="Username"
-                            id="form1"
-                            type="text"
-                            name="username"
-                            required={true}
-                            value={user.username}
-                            onChange={handleInputChanged}
-                     />
-
-                     <MDBInput
-                            wrapperClass="mb-4"
-                            label="Email address"
-                            id="form2"
-                            type="email"
-                            name="email"
-                            required={true}
-                            value={user.email}
-                            onChange={handleInputChanged}
-                     />
-                     <MDBInput
-                            wrapperClass="mb-4"
-                            label="Password"
-                            id="form3"
-                            type="password"
-                            name="password"
-                            required={true}
-                            value={user.password}
-                            onChange={handleInputChanged}
-                     />
-
-                     <MDBInput
-                            wrapperClass="mb-4"
-                            label="Contact"
-                            id="form4"
-                            type="text"
-                            name="contact"
-                            required={true}
-                            value={user.contact}
-                            onChange={handleInputChanged}
-                     />
-
-                     <button onClick={handleLogin} className="mb-4 btn btn-primary">
-                            Sign Up
-                     </button>
-
-                     <div className="text-center">
-                            <p>
-                                   Already Signed up? <Link to="/login">login</Link>
-                            </p>
-                     </div>
+                     <MDBRow className="justify-content-center">
+                            <MDBCol md="8" lg="6" className="text-center">
+                                   <img width="70" height="70" src={Gift} alt="Logo" className="mb-4" />
+                                   <h2 className="mb-4">Gift.io</h2>
+                            </MDBCol>
+                     </MDBRow>
+                     <MDBRow className="justify-content-center">
+                            <MDBCol md="8" lg="6">
+                                   <h3 className="mb-4">Sign Up</h3>
+                                   <MDBInput
+                                          wrapperClass='mb-4'
+                                          label='Username'
+                                          id='form1'
+                                          type='text'
+                                          name='username'
+                                          required={true}
+                                          value={user.username}
+                                          onChange={handleInputChanged}
+                                   />
+                                   <MDBInput
+                                          wrapperClass='mb-4'
+                                          label='Email address'
+                                          id='form2'
+                                          type='email'
+                                          name='email'
+                                          required={true}
+                                          value={user.email}
+                                          onChange={handleInputChanged}
+                                   />
+                                   <MDBInput
+                                          wrapperClass='mb-4'
+                                          label='Password'
+                                          id='form3'
+                                          type='password'
+                                          name='password'
+                                          required={true}
+                                          value={user.password}
+                                          onChange={handleInputChanged}
+                                   />
+                                   <MDBInput
+                                          wrapperClass='mb-4'
+                                          label='Contact'
+                                          id='form4'
+                                          type='text'
+                                          name='contact'
+                                          required={true}
+                                          value={user.contact}
+                                          onChange={handleInputChanged}
+                                   />
+                                   <button onClick={handleLogin} className="mb-4 btn btn-primary">
+                                          Sign Up with Gift.io
+                                   </button>
+                                   <div className="text-center">
+                                          <p>
+                                                 Already Signed up? <Link to="/login">Login</Link>
+                                          </p>
+                                   </div>
+                            </MDBCol>
+                     </MDBRow>
               </MDBContainer>
+
        );
 }
