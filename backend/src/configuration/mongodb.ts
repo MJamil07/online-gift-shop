0e8d05eef4e18f5d2d4b852b6dca79bcefb9c4fc
MJamil07 <kingivan223@gmail.com>
2024-02-12 10:42:05 +0530

    (setup) cloud mongodb

--- backend/src/configuration/mongodb.ts
+++ backend/src/configuration/mongodb.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const createDb = async () => {
-       await mongoose.connect('mongodb://127.0.0.1:27017/gift_shop' , {retryWrites : true , w : "majority" })
+       await mongoose.connect('mongodb+srv://klenty_assign:assignment-gift@gift-cluster.lcxdlnr.mongodb.net/gift_shop' , {retryWrites : true , w : "majority" })
        .then(()=> {
               console.log('Mongodb was connected');
        })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,3 +32,4 @@ app.use('*' , (request : Request , response : Response) => {
 })
 
 export default app
+ 
